9cf15b99337945e257d63a587d526eed506d83c2
YaoEnn <hongdieuan.712000@gmail.com>
2023-04-10 03:31:03 +0700

    Add Order_By Func

--- BackEnd/routes/trip_services.py
+++ BackEnd/routes/trip_services.py
@@ -63,23 +63,272 @@ def display_all_trip():
 
     # Filter trips from tomorrow
     trips = Trip.query.filter(Trip.departure_day > today).all()
-    
-    #Return all Trips that satisfy requirements
-    result = []
-    for trip in trips_today:
-        data = {'id': trip.id, 'start': trip.start, 'end': trip.end, 'departure_time': trip.departure_time.strftime("%H:%M"), 
-                            'arrived_time': trip.arrived_time, 'price': trip.price, 'departure_day': trip.departure_day, 'car_type': trip.car_type.name}
-        result.append(data)
-        
-    for trip in trips:
-        data = {'id': trip.id, 'start': trip.start, 'end': trip.end, 'departure_time': trip.departure_time.strftime("%H:%M"), 
-                            'arrived_time': trip.arrived_time, 'price': trip.price, 'departure_day': trip.departure_day, 'car_type': trip.car_type.name}
-        result.append(data)
 
-    return jsonify({
-        'status':'OK',
-        'msg': result
-    })
+    all_trips = trips_today + trips
+    
+    return ReturnTrips(all_trips)
+
+@app.route('/trip/order-by-user', methods = ['GET', 'POST'])
+def order_by_user():
+    #get current time
+    current_time = datetime.combine(datetime.today(), datetime.now().time())
+
+    #get current date
+    today = datetime.today().date()
+
+    #get data from Front-end
+    data = request.json
+    if (data and 'start' in data and 'end' in data and 'departure_time' in data
+         and 'price' in data and 'car_type' in data and 'departure_day' in data):
+        start = data['start']
+        end = data['end']
+        departure_time = data['departure_time']
+        price = data['price']
+        car_type = data['car_type']
+        departure_day = data['departure_day']
+    else:
+        return jsonify({
+            'status':'FAIL',
+            'err':'Missing Parameters'
+        })
+    
+    #user order "departure" from all trips 
+    if start is not None and end is None and departure_time is None and price is None and car_type is None and departure_day is None:
+        #Filter today trips later than current time 1 hour
+        trips_today = Trip.query.filter(Trip.start == start, Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
+                                Trip.departure_day == today).all()
+
+        # Filter trips from tomorrow
+        trips_future = Trip.query.filter(Trip.start == start, Trip.departure_day > today).all()
+
+        all_trips = trips_today + trips_future
+        return ReturnTrips(all_trips)
+    
+    #user order "departure" from searched trips
+    if (start is not None and end is not None and departure_day is not None 
+        and departure_time is None and price is None and car_type is None):
+
+        if departure_day == today:
+            #Filter today trips later than current time 1 hour
+            trips = Trip.query.filter(Trip.start == start, Trip.end == end,
+                                    Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
+                                    Trip.departure_day == today).all()
+
+        else:
+            trips = Trip.query.filter(Trip.start == start, Trip.end == end,
+                                Trip.departure_day == departure_day).all()
+        
+        return ReturnTrips(trips)
+    
+    #user order "departure" from recommended trips
+    if (start is not None and end is not None and departure_day is None
+        and departure_time is None and price is None and car_type is None):
+
+        return RecommendTrip(start, end)
+    
+    #user order "car_type" from all trips
+    if (start is None and end is None and departure_day is None
+        and departure_time is None and price is None and car_type is not None):
+
+        #set car_id
+        car_id = 3
+        if car_type == "Limousine":
+            car_id = 1
+        if car_type == "Bus":
+            car_id = 2
+        
+        #Filter today trips later than current time 1 hour
+        trips_today = Trip.query.filter(Trip.car_id ==  car_id,
+                                Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
+                                Trip.departure_day == today).all()
+
+        # Filter trips from tomorrow
+        trips = Trip.query.filter(Trip.car_id == car_id, Trip.departure_day > today).all()
+        
+        all_trips = trips_today + trips
+        
+        #Return all Trips that satisfy requirements
+        return ReturnTrips(all_trips)
+    
+    #user order 'car_type' from searched trips
+    if (start is None and end is not None and departure_day is not None
+        and departure_time is None and price is None and car_type is not None):
+
+        #set car_id
+        car_id = 3
+        if car_type == "Limousine":
+            car_id = 1
+        if car_type == "Bus":
+            car_id = 2
+        
+        #Filter today trips later than current time 1 hour
+        trips_today = Trip.query.filter(Trip.departure_day == departure_day, Trip.end == end, Trip.car_id ==  car_id,
+                                Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
+                                Trip.departure_day == today).all()
+
+        # Filter trips from tomorrow
+        trips = Trip.query.filter(Trip.departure_day == departure_day, Trip.end == end, 
+                                  Trip.car_id == car_id, Trip.departure_day > today).all()
+        
+        all_trips = trips_today + trips
+        
+        return ReturnTrips(all_trips)
+    
+    #user order "car_type" from recommened trip:
+    if (start is not None and end is not None and departure_day is None
+            and departure_time is None and price is None and car_type is not None):
+
+        #set car_id
+        car_id = 3
+        if car_type == "Limousine":
+            car_id = 1
+        if car_type == "Bus":
+            car_id = 2
+        
+        #Filter today trips later than current time 1 hour
+        trips_today = Trip.query.filter(Trip.start == start, Trip.end == end, Trip.car_id ==  car_id,
+                                Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
+                                Trip.departure_day == today).all()
+
+        # Filter trips from tomorrow
+        trips = Trip.query.filter(Trip.start == start, Trip.end == end, 
+                                Trip.car_id == car_id, Trip.departure_day > today).all()
+        
+        all_trips = trips_today + trips
+        
+        return ReturnTrips(all_trips)
+
+    #user order "low - High price" from all trips
+    if (start is None and end is None and price is not None and
+         departure_time is None and car_type is None and departure_day is None):
+        #Filter today trips later than current time 1 hour
+        trips_today = Trip.query.filter(Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
+                                Trip.departure_day == today).all()
+
+        # Filter trips from tomorrow
+        trips = Trip.query.filter(Trip.departure_day > today).all()
+        
+        all_trips = trips + trips_today
+        #sort by day firstly
+        sorted_day = sorted(all_trips, key=lambda trip: trip.departure_day)
+        
+        if price == "Price Ascend":
+            #sort by asc price
+            sorted_price = sorted(sorted_day, key=lambda trip: trip.price)
+            
+        else:
+            #sort by desc price
+            sorted_price = sorted(sorted_day, key=lambda trip: trip.price, reverse = True)
+
+        return ReturnTrips(sorted_price)
+
+    #user order "low - High price" from searched trips
+    if (start is None and end is not None and price is not None and
+         departure_time is None and car_type is None and departure_day is not None):
+        
+        trips = Trip.query.filter(Trip.departure_day == departure_day, Trip.end == end).all()
+
+        if price == "Price Ascend":
+            #sort by asc price
+            sorted_price = sorted(trips, key=lambda trip: trip.price)
+            
+        else:
+            #sort by desc price
+            sorted_price = sorted(trips, key=lambda trip: trip.price, reverse = True)
+
+        return ReturnTrips(sorted_price)
+    
+    #user order "low - High price" from recommended trips
+    if (start is not None and end is not None and price is not None and
+         departure_time is None and car_type is None and departure_day is None):
+        #Filter today trips later than current time 1 hour
+        trips_today = Trip.query.filter(Trip.start ==  start, Trip.end == end,
+                                        Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
+                                        Trip.departure_day == today).all()
+
+        # Filter trips from tomorrow
+        trips = Trip.query.filter(Trip.start ==  start, Trip.end == end, Trip.departure_day > today).all()
+        
+        all_trips = trips + trips_today
+        #sort by day firstly
+        sorted_day = sorted(all_trips, key=lambda trip: trip.departure_day)
+        
+        if price == "Price Ascend":
+            #sort by asc price
+            sorted_price = sorted(sorted_day, key=lambda trip: trip.price)
+            
+        else:
+            #sort by desc price
+            sorted_price = sorted(sorted_day, key=lambda trip: trip.price, reverse = True)
+
+        return ReturnTrips(sorted_price)
+    
+    #user order "Earliest - Latest" from all trips
+    if (start is None and end is None and price is None and
+         departure_time is not None and car_type is None and departure_day is None):
+        #Filter today trips later than current time 1 hour
+        trips_today = Trip.query.filter(Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
+                                Trip.departure_day == today).all()
+
+        # Filter trips from tomorrow
+        trips = Trip.query.filter(Trip.departure_day > today).all()
+        
+        all_trips = trips + trips_today
+        #sort by day firstly
+        sorted_day = sorted(all_trips, key=lambda trip: trip.departure_day)
+        
+        if departure_time == "Earliest":
+            #sort by asc time
+            sorted_time = sorted(sorted_day, key=lambda trip: trip.departure_time)
+            
+        else:
+            #sort by desc time
+            sorted_time = sorted(sorted_day, key=lambda trip: trip.departure_time, reverse = True)
+
+        return ReturnTrips(sorted_time)
+    
+    #user order "Earliest - Latest" from all searched trips
+    if (start is None and end is not None and price is None and
+         departure_time is not None and car_type is None and departure_day is not None):
+        
+        trips = Trip.query.filter(Trip.departure_day == departure_day, Trip.end == end).all()
+
+        if departure_time == "Earliest":
+            #sort by asc time
+            sorted_time = sorted(trips, key=lambda trip: trip.departure_time)
+            
+        else:
+            #sort by desc time
+            sorted_time = sorted(trips, key=lambda trip: trip.departure_time, reverse = True)
+
+        return ReturnTrips(sorted_time)
+    
+    #user order "Earliest - Latest" from recommended trips
+    if (start is not None and end is not None and price is None and
+        departure_time is not None and car_type is None and departure_day is None):
+
+        #Filter today trips later than current time 1 hour
+        trips_today = Trip.query.filter(Trip.start ==  start, Trip.end == end,
+                                        Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
+                                        Trip.departure_day == today).all()
+
+        # Filter trips from tomorrow
+        trips = Trip.query.filter(Trip.start ==  start, Trip.end == end, Trip.departure_day > today).all()
+        
+        all_trips = trips + trips_today
+        #sort by day firstly
+        sorted_day = sorted(all_trips, key=lambda trip: trip.departure_day)
+        
+        if departure_time == "Earliest":
+            #sort by asc time
+            sorted_time = sorted(sorted_day, key=lambda trip: trip.departure_time)
+            
+        else:
+            #sort by desc time
+            sorted_time = sorted(sorted_day, key=lambda trip: trip.departure_time, reverse = True)
+
+        return ReturnTrips(sorted_time)
+    
 
 #DISTRICT 1 ----> BAOLOC TRIP    
 @app.route('/recommend-trip/Q1-BL', methods = ['POST'])
@@ -122,7 +371,7 @@ def VT_BT_Trip():
     return RecommendTrip('VungTau City', 'BinhTan District, HCMC')
 
 #---------------Function Use For Trips Recommendation Filter base on Current DateTime---------------#
-def RecommendTrip(begin, des):
+def RecommendTrip(start, end):
     #get current time
     current_time = datetime.combine(datetime.today(), datetime.now().time())
 
@@ -130,22 +379,32 @@ def RecommendTrip(begin, des):
     today = datetime.today().date()
     
     #Filter today trips later than current time 1 hour
-    trips_today = Trip.query.filter(Trip.start == begin, Trip.end == des,
+    trips_today = Trip.query.filter(Trip.start == start, Trip.end == end,
                               Trip.departure_time >= (current_time + timedelta(hours=1)).time(),
                               Trip.departure_day == today).all()
 
     # Filter trips from tomorrow
-    trips = Trip.query.filter(Trip.start == begin, Trip.end == des,
+    trips = Trip.query.filter(Trip.start == start, Trip.end == end,
                               Trip.departure_day > today).all()
+    
+    all_trips = trips + trips_today
     
     #Return all Trips that satisfy requirements
     result = []
-    for trip in trips_today:
+    for trip in all_trips:
         data = {'id': trip.id, 'start': trip.start, 'end': trip.end, 'departure_time': trip.departure_time.strftime("%H:%M"), 
                             'arrived_time': trip.arrived_time, 'price': trip.price, 'departure_day': trip.departure_day, 'car_type': trip.car_type.name}
         result.append(data)
-        
-    for trip in trips:
+
+    return jsonify({
+        'status':'OK',
+        'msg': result
+    })
+
+def ReturnTrips(all_trips):
+    #Return all Trips that satisfy requirements
+    result = []
+    for trip in all_trips:
         data = {'id': trip.id, 'start': trip.start, 'end': trip.end, 'departure_time': trip.departure_time.strftime("%H:%M"), 
                             'arrived_time': trip.arrived_time, 'price': trip.price, 'departure_day': trip.departure_day, 'car_type': trip.car_type.name}
         result.append(data)
