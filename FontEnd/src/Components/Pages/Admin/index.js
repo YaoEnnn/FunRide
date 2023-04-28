2bc563ebce9c3c789f1b19cea3007a079cf82ba9
CaNeoooo <93911422+CaNeoooo@users.noreply.github.com>
2023-04-28 13:45:02 +0700

    1. finished the admin page 2. finished the addAdmin page

--- FontEnd/src/Components/Pages/Admin/index.js
+++ FontEnd/src/Components/Pages/Admin/index.js
@@ -1,16 +1,99 @@
 import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import axios from "axios";
+import AnimatedOutlet from "../../AnimatedOutlet";
+import Popup from "./PopUp/popup";
+import { success } from "../../lib/toast";
+import { useOutlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
-  const [name, setName] = useState("");
+  const [showAdmin, setShowAdmin] = useState([]);
+  const [showPopUp, setShowPopUp] = useState(false);
+  const navigate = useNavigate();
+  const [currentAdmin, setCurrentAdmin] = useState(null);
+  const handleButtonClick = () => {
+    setShowPopUp(true);
+  };
+  const handleClosePopUp = () => {
+    setShowPopUp(false);
+  };
+  const confbutton = (a) => {
+    axios.delete(`manager/delete-admin/${a.id}`).then((resp) => {
+      if (resp.data.status === "OK") {
+        success("Successfully deleted user");
+      }
+    });
+    setShowPopUp(false);
+  };
 
   useEffect(() => {
-    axios.post("admin/my-profile").then((resp) => {
-      console.log(resp);
-      setName(resp.data.msg.name);
+    axios.post("manager/display-all-admin").then((resp) => {
+      console.log(resp.data.msg);
+      setShowAdmin(resp.data.msg);
     });
-  });
-  return <h1>This is the admin page, hi {name}</h1>;
+  }, []);
+
+  const outlet = useOutlet();
+  if (outlet) {
+    return outlet;
+  }
+
+  return (
+    <AnimatedOutlet>
+      <div className={styles.base}>
+        <div className={styles.header}>
+          <h1>--Display Admin--</h1>
+          <button
+            onClick={() => {
+              navigate("add");
+            }}
+          >
+            Add a new admin
+          </button>
+        </div>
+        <div className={styles.table}>
+          <div>
+            <div>
+              <div>
+                <label>Name</label>
+                <label>Email</label>
+                <label>Actions</label>
+              </div>
+            </div>
+            <div>
+              {showAdmin.map((e) => {
+                return (
+                  <div className={styles.doc} key={e.id}>
+                    <label>{e.name}</label>
+                    <label>{e.email}</label>
+                    <label
+                      onClick={() => {
+                        setShowPopUp(true);
+                        setCurrentAdmin(e);
+                      }}
+                    >
+                      Delete
+                    </label>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        {showPopUp && (
+          <Popup
+            handleClose={handleClosePopUp}
+            announcement={"Warning"}
+            confirm={"Confirm"}
+            yousure={"Are you sure you want to delete this admin?"}
+            confbutton={() => {
+              confbutton(currentAdmin);
+            }}
+          ></Popup>
+        )}
+      </div>
+    </AnimatedOutlet>
+  );
 };
 export default Admin;
