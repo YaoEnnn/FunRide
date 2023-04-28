620f7d176cc783f7f6d66c208a34d49868fe13a7
CaNeoooo <93911422+CaNeoooo@users.noreply.github.com>
2023-04-28 23:24:29 +0700

    asdsofij

--- FontEnd/src/Components/Pages/searchTrip/index.js
+++ FontEnd/src/Components/Pages/searchTrip/index.js
@@ -5,13 +5,179 @@ import { useState, useEffect } from "react";
 import DropDown from "../../DropDown";
 import axios from "axios";
 import { error } from "../../lib/toast";
+import Popup from "./PopUp/popup";
 
 function SearchTrip() {
   const [date, setDate] = useState();
+  const [currentTrip, setCurrentTrip] = useState(null);
   const [value, setvalue] = useState(null);
   const [end, setEnd] = useState("");
   const [trip, setTrip] = useState([]);
   const [booking, setBooking] = useState([]);
+  const [count, setCount] = useState(20);
+  const loadMore = () => {
+    setCount(count + 20);
+  };
+
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  const handleClosePopUp = () => {
+    setShowPopUp(false);
+  };
+  const confbutton = (a) => {};
+
+  const sortArray = [
+    "Not filterd",
+    "Departure from District 1, HCMC",
+    "Departure from Binh Tan District, HCMC",
+    "Order by Limousine",
+    "Order by Bus",
+    "Order by Sleeper Bus",
+    "Low - High Price",
+    "High - Low Price",
+    "Earliest - Latest",
+    "Latest - Earliest",
+  ];
+  const [sort, setSort] = useState("");
+  const handleSortChange = (event) => {
+    setSort(event.target.value);
+    console.log(event.target.value);
+    if (event.target.value === "Departure from District 1, HCMC") {
+      axios
+        .post("trip/order-by-user", {
+          start: "District 1, HCMC",
+          end: null,
+          departure_time: null,
+          price: null,
+          car_type: null,
+          departure_day: null,
+        })
+        .then((resp) => {
+          console.log(resp.data.msg);
+          setTrip(resp.data.msg);
+        });
+    }
+    if (event.target.value === "Departure from Binh Tan District, HCMC") {
+      axios
+        .post("trip/order-by-user", {
+          start: "BinhTan District, HCMC",
+          end: null,
+          departure_time: null,
+          price: null,
+          car_type: null,
+          departure_day: null,
+        })
+        .then((resp) => {
+          console.log(resp.data.msg);
+          setTrip(resp.data.msg);
+        });
+    }
+    if (event.target.value === "Order by Limousine") {
+      axios
+        .post("trip/order-by-user", {
+          start: null,
+          end: null,
+          departure_time: null,
+          price: null,
+          car_type: "Limousine",
+          departure_day: null,
+        })
+        .then((resp) => {
+          console.log(resp.data.msg);
+          setTrip(resp.data.msg);
+        });
+    }
+    if (event.target.value === "Order by Bus") {
+      axios
+        .post("trip/order-by-user", {
+          start: null,
+          end: null,
+          departure_time: null,
+          price: null,
+          car_type: "Bus",
+          departure_day: null,
+        })
+        .then((resp) => {
+          console.log(resp.data.msg);
+          setTrip(resp.data.msg);
+        });
+    }
+    if (event.target.value === "Order by Sleeper Bus") {
+      axios
+        .post("trip/order-by-user", {
+          start: null,
+          end: null,
+          departure_time: null,
+          price: null,
+          car_type: "Sleeper-Bus",
+          departure_day: null,
+        })
+        .then((resp) => {
+          console.log(resp.data.msg);
+          setTrip(resp.data.msg);
+        });
+    }
+    if (event.target.value === "Low - High Price") {
+      axios
+        .post("trip/order-by-user", {
+          start: null,
+          end: null,
+          departure_time: null,
+          price: "Price Ascend",
+          car_type: null,
+          departure_day: null,
+        })
+        .then((resp) => {
+          console.log(resp.data.msg);
+          setTrip(resp.data.msg);
+        });
+    }
+    if (event.target.value === "High - Low Price") {
+      axios
+        .post("trip/order-by-user", {
+          start: null,
+          end: null,
+          departure_time: null,
+          price: "Price Descend",
+          car_type: null,
+          departure_day: null,
+        })
+        .then((resp) => {
+          console.log(resp.data.msg);
+          setTrip(resp.data.msg);
+        });
+    }
+    if (event.target.value === "Earliest - Latest") {
+      axios
+        .post("trip/order-by-user", {
+          start: null,
+          end: null,
+          departure_time: "Earliest",
+          price: null,
+          car_type: null,
+          departure_day: null,
+        })
+        .then((resp) => {
+          console.log(resp.data.msg);
+          setTrip(resp.data.msg);
+        });
+    }
+    if (event.target.value === "Latest - Earliest") {
+      axios
+        .post("trip/order-by-user", {
+          start: null,
+          end: null,
+          departure_time: "Latest",
+          price: null,
+          car_type: null,
+          departure_day: null,
+        })
+        .then((resp) => {
+          console.log(resp.data.msg);
+          setTrip(resp.data.msg);
+        });
+    }
+  };
 
   const endArray = [
     "Unknown",
@@ -28,7 +194,6 @@ function SearchTrip() {
 
   useEffect(() => {
     axios.post("trip/display-all").then((resp) => {
-      console.log(resp.data.msg);
       setTrip(resp.data.msg);
     });
   }, []);
@@ -78,10 +243,20 @@ function SearchTrip() {
         <div className={styles.listView}>
           <section className={styles.sort}>
             <h1>--Sorting--</h1>
-            <select></select>
+            <select
+              value={sort}
+              onChange={handleSortChange}
+              defaultValue={sortArray}
+            >
+              {sortArray.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
           </section>
           <section className={styles.cardview}>
-            {trip.map((e) => {
+            {trip.slice(0, count).map((e) => {
               return (
                 <div className={styles.doc} key={e.id}>
                   <div>
@@ -118,14 +293,36 @@ function SearchTrip() {
                       <div>
                         <label>Price: {e.price} vnđ</label>
                       </div>
-                      <button>Confirm</button>
+                      <button
+                        onClick={() => {
+                          setCurrentTrip(e);
+                          setShowPopUp(true);
+                          console.log(e);
+                        }}
+                      >
+                        Book
+                      </button>
                     </div>
                   </div>
                 </div>
               );
             })}
+            {count < trip.length && (
+              <button onClick={loadMore}>Load More</button>
+            )}
           </section>
         </div>
+        {showPopUp && (
+          <Popup
+            handleClose={handleClosePopUp}
+            announcement={"Warning"}
+            confirm={"Confirm"}
+            yousure={"Are you sure you want to delete this admin?"}
+            confbutton={() => {
+              confbutton(currentTrip);
+            }}
+          ></Popup>
+        )}
       </div>
     </AnimatedOutlet>
   );
